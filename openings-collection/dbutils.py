b0c4390762792007c2657d6a8ef26617be972100
Maciej Groszyk <maciej.groszyk8@gmail.com>
2022-11-08 04:00:52 +0100

    add searchbox to games

--- openings-collection/dbutils.py
+++ openings-collection/dbutils.py
@@ -3,6 +3,8 @@ import os
 from dotenv import load_dotenv
 from flask_pymongo import PyMongo, ObjectId
 from flask import current_app as app
+import sys
+
 
 load_dotenv()
 
@@ -14,11 +16,14 @@ def db():
     mongo = PyMongo(app)
     return mongo.db
 
+
 def select_opening(key, value):
     return list(db().openings.find({key: value}))
 
+
 def select_all_opening():
     return list(db().openings.find())
+
 
 def add_opening(name, eco, moves):
     _openings_names = db().openings.distinct("name")
@@ -26,20 +31,51 @@ def add_opening(name, eco, moves):
     if item["name"] not in _openings_names and item["name"]:
         db().openings.insert_one(item)
 
+
 def delete_opening(op_name):
     db().openings.delete_one({"name": op_name})
+
 
 def edit_opening(name, new_name, eco_code, moves):
     new_record = {"$set": {"name": new_name, "eco_code": eco_code, "moves": moves}}
     db().openings.find_one_and_update({"name": name}, new_record)
 
+
 def add_game(white, black, opening, moves, result):
-    game = {"white": white, "black": black, "opening": opening, "moves": moves, "result": result}
+    game = {
+        "white": white,
+        "black": black,
+        "opening": opening,
+        "moves": moves,
+        "result": result,
+    }
     if moves:
         db().games.insert_one(game)
+
 
 def select_all_games():
     return list(db().games.find())
 
+
+def select_game(_id):
+    return list(db().games.find({"_id": ObjectId(_id)}))
+
+
 def delete_game(_id):
     db().games.delete_one({"_id": ObjectId(_id)})
+
+
+def edit_game(_id, white, black, moves, result):
+    new_record = {
+        "$set": {"white": white, "black": black, "moves": moves, "result": result}
+    }
+    db().games.find_one_and_update({"_id": ObjectId(_id)}, new_record)
+
+
+def searched_games(query):
+    output = []
+    for key in ["white", "black", "opening"]:
+        result = list(db().games.find({key: query}))
+        if result:
+            output.append(result[0])
+    return output
