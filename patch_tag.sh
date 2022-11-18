1adb449de283e8cd21ab36bf137f1caabd8fff8c
Maciej Groszyk <maciej.groszyk8@gmail.com>
2022-11-18 22:52:57 +0100

    3.1 fix

--- patch_tag.sh
+++ patch_tag.sh
@@ -2,5 +2,5 @@ kubectl patch --local -f ./nginx/values.yaml -p '{"image":{"tag":"${1}"' -o yaml
 pushd chess-openings-helmcharts
 git add -A
 git commit -m "change tag to ${1}"
-git push https://maciejgrosz:${2}@github.com/maciejgrosz/chess-openings-collection.git 
+git push https://maciejgrosz:${2}@github.com/maciejgrosz/chess-openings-helmcharts.git
 popd 
