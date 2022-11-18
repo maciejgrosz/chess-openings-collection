b2424f93690a6355dc2b71e5b5ba6fcddd3f6d72
Maciej Groszyk <maciej.groszyk8@gmail.com>
2022-11-19 00:40:53 +0100

    6.0 test

--- patch_tag.sh
+++ patch_tag.sh
@@ -1,6 +1,7 @@
 pushd chess-openings-helmcharts
 TAG=$1
 yq -i '.image.tag = strenv(TAG)' ./nginx/values.yaml
+yq -i '.image.tag = strenv(TAG)' ./nginx/charts/chess/values.yaml
 git add -A
 git commit -m "change tag to ${1}"
 git push https://maciejgrosz:${2}@github.com/maciejgrosz/chess-openings-helmcharts.git
