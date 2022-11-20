64d14b137815d7263afdca8bb3733b59b00ac511
Maciej Groszyk <maciej.groszyk8@gmail.com>
2022-11-20 03:31:29 +0100

    delete adminauth

--- openings-collection/dbutils.py
+++ openings-collection/dbutils.py
@@ -6,9 +6,9 @@ from flask import current_app as app
 def db():
     app.config[
         "MONGO_URI"
-    ] = f'mongodb://{os.environ["MONGODB_USERNAME"]}:{os.environ["MONGODB_PASSWORD"]}@{os.environ["MONGODB_HOSTNAME"]}:27017/{os.environ["MONGODB_DATABASE"]}?authSource=admin'
+    ] = f'mongodb://{os.environ["MONGODB_USERNAME"]}:{os.environ["MONGODB_PASSWORD"]}@{os.environ["MONGODB_HOSTNAME"]}:27017/{os.environ["MONGODB_DATABASE"]}'
     mongo = PyMongo(app)
-    return mongo.db
+    return mongo.db 
 
 
 def select_opening(key, value):
